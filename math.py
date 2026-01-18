daef9d40d400573c708b8779549741b209f8f5f8
swapnil272425-hub <swapnil272425@gmail.com>
2026-01-18 21:47:40 +0530

    Update math.py

--- math.py
+++ math.py
@@ -1,4 +1,4 @@
-    a = float(input("Enter first number: "))
-    b = float(input("Enter second number: "))
-    print(f"Addition: {a + b}")
-    print(f"Subtraction: {a - b}")
+a = float(input("Enter first number: "))   # Takes input from user and converts it to a float
+b = float(input("Enter second number: "))  # Takes second input from user and converts it to a float
+print(f"Addition: {a + b}")                # Prints the sum of a and b
+print(f"Subtraction: {a - b}")             # Prints the difference of a and b
