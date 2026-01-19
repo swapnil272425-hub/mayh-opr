96f52efd3429ec9656d5635c44810e47f5105960
swapnil272425-hub <swapnil272425@gmail.com>
2026-01-19 11:56:53 +0530

    Delete math.py
    delete message

--- math.py
+++
@@ -1,4 +0,0 @@
-a = float(input("Enter first number: "))   # Takes input from user and converts it to a float
-b = float(input("Enter second number: "))  # Takes second input from user and converts it to a float
-print(f"Addition: {a + b}")                # Prints the sum of a and b
-print(f"Subtraction: {a - b}")             # Prints the difference of a and b
